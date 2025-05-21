8d1696d87eff65708dea76371696b14079f94fed
jasonlryan <jason.ironlion@gmail.com>
2025-05-21 07:31:16 +0100

    Add Vercel Analytics integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import { AuthProvider } from "./hooks/useAuth";
 import { useAuth } from "./hooks/useAuth";
 import { Layout } from "./components/Layout";
@@ -142,6 +143,7 @@ function App() {
           />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </AuthProvider>
   );
 }
